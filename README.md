a3d2e3edf254a0e362704b85f7462fe730fa0c95
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-03-01 15:24:15 +0900

    The first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cached image optimizer
+
+This library fetches images via HTTP(S), optimizes them and stores into some storages.
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cached-image-optimizer",
+    version="0.0.1",
+    install_requires=[
+        "requests>=2.25.1",
+        "ratelimit>=2.2.1",
+        "Pillow>=8.1.0",
+    ],
+    extras_require={
+        "s3": ["boto3>=1.17.17"],
+    },
+    author="Tasuku SUENAGA a.k.a. gunyarakun",
+    author_email="tasuku-s-github@titech.ac",
+    description="cached image optimizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gunyarakun/cached-image-optimizer",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
