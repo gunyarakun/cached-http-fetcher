ca8c910bbc1794cbb124fce8a7c4058571e6b6d5
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-03-04 18:28:19 +0900

    Implement image opmizier

--- src/fetcher.py
+++ src/fetcher.py
@@ -2,6 +2,7 @@ from typing import Dict, Set, Iterable
 import warnings
 import requests
 import multiprocessing
+from dataclasses import dataclass
 from ratelimit import limits, RateLimitException
 from backoff import on_exception, expo
 
@@ -12,6 +13,13 @@ warnings.simplefilter("ignore", InsecureRequestWarning)
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
 }
+
+@dataclass(frozen=True)
+class FetchedImage:
+    url: str
+    image: bytes
+
+
 
 @on_exception(expo, RateLimitException, max_tries=8)
 @limits(calls=10, period=60) # 10 requests per 1 minute
@@ -25,41 +33,85 @@ def requests_get(url):
 
 
 class FetchWorker(multiprocessing.Process):
-    def __init__(self, job_queue, *, logger):
+    def __init__(self, url_queue, image_queue):
         super().__init__()
-        self._job_queue = job_queue
-        self._logger = logger
+        self._url_queue = url_queue
+        self._image_queue = image_queue
+        self._logger = multiprocessing.get_logger()
 
 
     def run(self):
         while True:
-            url_set = self._job_queue.get()
+            url_set = self._url_queue.get()
             if url_set is None:
                 break
 
             for url in url_set:
                 try:
                     response = requests_get(url)
+                    self._image_queue.put(FetchedImage(url=url, image=response.content))
                 except requests.RequestException as e:
                     self._logger.warning(str(e))
                 except Exception as e:
                     self._logger.warning(str(e))
 
 
-def fetch_images(url_dict: Dict[str, Set[str]], *, logger):
-    jobs = []
-    job_queue = multiprocessing.Queue()
+class OptimizeWorker(multiprocessing.Process):
+    def __init__(self, image_queue):
+        super().__init__()
+        self._image_queue = image_queue
+        self._logger = multiprocessing.get_logger()
 
-    for i in range(16):
-        p = FetchWorker(job_queue, logger=logger)
-        jobs.append(p)
+
+    def run(self):
+        while True:
+            fetched_image = self._image_queue.get()
+            if fetched_image is None:
+                break
+
+            print(f"optimize: {fetched_image.url}")
+            self._logger.info(f"optimize: {fetched_image.url}")
+
+
+def fetch_images(url_dict: Dict[str, Set[str]], *, logger):
+    fetch_jobs = []
+    optimize_jobs = []
+    url_queue = multiprocessing.Queue()
+    image_queue = multiprocessing.Queue()
+    log_queue = multiprocessing.Queue()
+
+    num_fetcher = multiprocessing.cpu_count() * 4
+    num_fetcher = 1
+    num_optimizer = multiprocessing.cpu_count()
+
+    for i in range(num_fetcher):
+        p = FetchWorker(url_queue, image_queue)
+        fetch_jobs.append(p)
         p.start()
 
+    for i in range(num_optimizer):
+        p = OptimizeWorker(image_queue)
+        optimize_jobs.append(p)
+        p.start()
+
+    url_count = 0
     for domain, url_set in url_dict.items():
-        job_queue.put(url_set)
+        url_queue.put(url_set)
+        url_count += len(url_set)
 
-    for j in jobs:
-        job_queue.put(None)
+    for j in fetch_jobs:
+        url_queue.put(None)
 
-    for j in jobs:
+    # Wait for fetching all the images
+    for j in fetch_jobs:
+        j.join()
+
+    logger.info(f"Fetched all urls: {url_count}")
+
+    # Now nobody puts an item into `image_queue`, so we adds a terminator.
+    for j in optimize_jobs:
+        image_queue.put(None)
+
+    # Wait for optimizing all the images
+    for j in optimize_jobs:
         j.join()
