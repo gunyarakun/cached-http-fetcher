4c648683313a74d589b0514c15d13d825fdd4fcd
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-03-24 23:54:14 +0900

    Test tochu

--- src/storage/redis.py
+++ src/storage/redis.py
@@ -9,11 +9,6 @@ class RedisStorage(StorageBase):
 
     def put(self, key: str, value: bytes, expire=None) -> None:
         self.client.set(key, value, ex=expire)
-        config = dict(ACL="bucket-owner-full-control", Bucket=self.bucket, Key=key, ContentType=content_type, Body=value)
-        if expire:
-            # FIXME: set proper expire
-            config["CacheControl"] = expire
-        return self.client.put_object(**config)
 
     def url_from_key(self, key: str) -> str:
         raise NotImplementedError
--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -2,19 +2,42 @@ import pytest
 
 from url_list import urls_per_domain
 from fetcher import fetch_images, fetch_images_single
-from storage import MemoryStorage
+from storage import MemoryStorage, S3Storage, RedisStorage
 
 
-def test_fetch_images(images, logger, requests_mock):
-    url_dict = urls_per_domain(images.keys())
+class S3Client:
+    def __init__(self):
+        self.dict = {}
 
-    meta_memory_storage = MemoryStorage()
-    image_memory_storage = MemoryStorage()
+    def get_object(self, **kwargs):
+        return self.dict[kwargs["Key"]]
 
+    def put_object(self, **kwargs):
+        self.dict[kwargs["Key"]] = kwargs
+
+
+class RedisClient:
+    def __init__(self):
+        self.dict = {}
+
+    def get(self, key):
+        return self.dict[key]
+
+    def set(self, key, value, expire=None):
+        self.dict[key] = value
+
+
+def test_fetch_images_single_memory(images, logger, requests_mock):
     for url, obj in images.items():
         requests_mock.add(
             requests_mock.GET, url, body=obj['image']
         )
+
+    url_dict = urls_per_domain(images.keys())
+
+    # memory storage
+    meta_memory_storage = MemoryStorage()
+    image_memory_storage = MemoryStorage()
 
     fetch_images_single(url_dict, meta_storage=meta_memory_storage, image_storage=image_memory_storage, logger=logger)
 
@@ -24,3 +47,22 @@ def test_fetch_images(images, logger, requests_mock):
     assert len(requests_mock.calls) == len(images)
     assert len(meta_storage) == len(images)
     assert len(image_storage) == len(images)
+
+    # TODO: Add more tests
+
+
+def test_fetch_images_redis_s3(images, logger, requests_mock):
+    for url, obj in images.items():
+        requests_mock.add(
+            requests_mock.GET, url, body=obj['image']
+        )
+
+    url_dict = urls_per_domain(images.keys())
+
+    # redis/s3 storage
+    meta_redis_storage = RedisStorage(redis_client=RedisClient())
+    image_s3_storage = S3Storage(bucket='test_bucket', boto3_s3_client=S3Client())
+
+    fetch_images_single(url_dict, meta_storage=meta_redis_storage, image_storage=image_s3_storage, logger=logger)
+
+    assert len(requests_mock.calls) == len(images)
