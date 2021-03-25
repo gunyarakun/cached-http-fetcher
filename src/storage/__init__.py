7ed38e698048d0b32380308eedbc4afda0ad0d0e
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-03-26 00:56:56 +0900

    Remove S3 and Redis client

--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -1,4 +1,2 @@
 from .base import StorageBase
 from .memory import MemoryStorage
-from .s3 import S3Storage
-from .redis import RedisStorage
--- tests/test_fetcher.py
+++ tests/test_fetcher.py
@@ -2,29 +2,7 @@ import pytest
 
 from url_list import urls_per_domain
 from fetcher import fetch_images, fetch_images_single
-from storage import MemoryStorage, S3Storage, RedisStorage
-
-
-class S3Client:
-    def __init__(self):
-        self.dict = {}
-
-    def get_object(self, **kwargs):
-        return self.dict[kwargs["Key"]]
-
-    def put_object(self, **kwargs):
-        self.dict[kwargs["Key"]] = kwargs
-
-
-class RedisClient:
-    def __init__(self):
-        self.dict = {}
-
-    def get(self, key):
-        return self.dict[key]
-
-    def set(self, key, value, expire=None):
-        self.dict[key] = value
+from storage import MemoryStorage
 
 
 def test_fetch_images_single_memory(images, logger, requests_mock):
@@ -49,20 +27,3 @@ def test_fetch_images_single_memory(images, logger, requests_mock):
     assert len(image_storage) == len(images)
 
     # TODO: Add more tests
-
-
-def test_fetch_images_redis_s3(images, logger, requests_mock):
-    for url, obj in images.items():
-        requests_mock.add(
-            requests_mock.GET, url, body=obj['image']
-        )
-
-    url_dict = urls_per_domain(images.keys())
-
-    # redis/s3 storage
-    meta_redis_storage = RedisStorage(redis_client=RedisClient())
-    image_s3_storage = S3Storage(bucket='test_bucket', boto3_s3_client=S3Client())
-
-    fetch_images_single(url_dict, meta_storage=meta_redis_storage, image_storage=image_s3_storage, logger=logger)
-
-    assert len(requests_mock.calls) == len(images)
