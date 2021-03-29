e451215cc43b4a65714cec6a3daa0e4fdc8f0629
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-03-30 00:52:20 +0900

    fsdfdsfds

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/gunyarakun/cached-image-optimizer",
     package_dir={"cached_image_optimizer": "src"},
-    packages=setuptools.find_packages("cached_image_optimizer"),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
