a46454346c421964ff5bb5d41253b02bcf721778
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-03-31 21:57:00 +0900

    Rename args

--- cached_http_fetcher/storage/base.py
+++ cached_http_fetcher/storage/base.py
@@ -17,17 +17,17 @@ class StorageBase(ABC):
 
 class ContentStorageBase(ABC):
     @abstractmethod
-    def get(self, key: str) -> Optional[bytes]:
+    def get(self, source_url: str) -> Optional[bytes]:
         pass
 
     @abstractmethod
-    def delete(self, key: str) -> None:
+    def delete(self, source_url: str) -> None:
         pass
 
     @abstractmethod
-    def put_content(self, key: str, value: bytes, content_type: Optional[str] = None, expire: Optional[int] = None) -> None:
+    def put_content(self, source_url: str, value: bytes, content_type: Optional[str] = None, expire: Optional[int] = None) -> None:
         pass
 
     @abstractmethod
-    def url_from_key(self, key: str) -> str:
+    def url_from_key(self, source_url: str) -> str:
         pass
--- cached_http_fetcher/storage/memory.py
+++ cached_http_fetcher/storage/memory.py
@@ -23,23 +23,23 @@ class ContentMemoryStorage(ContentStorageBase):
     def __init__(self, **kwargs):
         self.dict = {}
 
-    def get(self, key: str) -> Optional[bytes]:
+    def get(self, source_url: str) -> Optional[bytes]:
         v = self.dict.get(key, None)
         if v is not None:
             return v["value"]
         return None
 
-    def delete(self, key: str) -> None:
+    def delete(self, source_url: str) -> None:
         del self.dict[key]
 
-    def put_content(self, key: str, value: bytes, content_type: Optional[str] = None, expire: Optional[int] = None) -> None:
+    def put_content(self, source_url: str, value: bytes, content_type: Optional[str] = None, expire: Optional[int] = None) -> None:
         self.dict[key] = {
             "value": value,
             "content_type": content_type,
             "expire": expire,
         }
 
-    def url_from_key(self, key: str) -> str:
+    def url_from_key(self, source_url: str) -> str:
         return f'memory:{key}'
 
     def dict_for_debug(self) -> dict:
