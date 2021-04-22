ce72ba53cfd0d4f156aab306dfdf14caf57d4eb4
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2021-04-22 23:59:29 +0900

    Do not delete Meta when the fetched content is not sutable for caching

--- cached_http_fetcher/entrypoint.py
+++ cached_http_fetcher/entrypoint.py
@@ -85,7 +85,8 @@ class ContentWorker(multiprocessing.Process):
                     self._content_storage,
                     logger=self._logger,
                 )
-                put_meta(fetched_response.url, meta, self._meta_storage)
+                if meta is not None:
+                    put_meta(fetched_response.url, meta, self._meta_storage)
             except Exception as ex:
                 self._logger.exception("Error on ContentWorker: %s", ex)
 
